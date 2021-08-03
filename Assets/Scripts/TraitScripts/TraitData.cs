ef4872163ff62f5cfac308ebe458cb52dceed64b
Progdruid <progdruid@gmail.com>
2021-08-03 23:40:31 +0300

    Registry's traitDatas and TraitData's Name deleted

--- Assets/Scripts/TraitScripts/TraitData.cs
+++ Assets/Scripts/TraitScripts/TraitData.cs
@@ -1,12 +1,5 @@
 
 public abstract class TraitData
 {
-    public readonly string Name;
-
     public abstract Trait CreateThisTrait(Structure _structure);
-
-    public TraitData (string _name)
-    {
-        Name = _name;
-    }
 }
